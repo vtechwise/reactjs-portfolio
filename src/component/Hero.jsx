0d7e6bea620da42c57fd52035ff0d1899700d81d
victor <oguntayovictor65@gmail.com>
2023-12-12 14:58:13 -0800

    hero section

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,33 +2,56 @@ import portfolioImg from "../assets/portfolioImg.jpg";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col gap-y-10 md:flex-row">
-      <div className="rounded-lg relative">
-        <img
-          src={portfolioImg}
-          className="w-screen h-[25rem] object-cover rounded-lg"
-          alt=""
-        />
-        <button className="btn btn-secondary absolute btn- rounded-full left-5  btn-sm bottom-20">
-          hello@victor
-        </button>
-        <p className="mt-4 flex justify-end items-center gap-x-2 text-xs text-content-neutral ">
-          {" "}
-          <span className="h-2 w-2 bg-secondary rounded-full"></span>Available
-          for new projects
+    <>
+      <div className="flex flex-col gap-y-10 md:flex-row gap-x-20 items-center">
+        <div className="rounded-lg relative">
+          <img
+            src={portfolioImg}
+            className="w-screen md:w-[35rem] h-[25rem] md:h-[32rem] object-cover rounded-lg"
+            alt=""
+          />
+          <button className="btn btn-secondary absolute btn- rounded-full left-5  btn-sm bottom-20">
+            hello@victor
+          </button>
+          <p className="mt-4 flex justify-end items-center gap-x-2 text-xs text-content-neutral ">
+            {" "}
+            <span className="h-2 w-2 bg-secondary rounded-full"></span>
+            Available for new projects
+          </p>
+        </div>
+        <div>
+          <h1 className="text-4xl md:text-5xl lg:text-6xl tracking-wide font-semibold">
+            I craft beautiful and unique products.
+          </h1>
+          <p className="mt-4 leading-7">
+            in my code every details serves a purpose- <br /> orchestrating a
+            digital masterpiece where user experience flow seamlessly{" "}
+          </p>
+          <button className="btn mt-6 btn-primary  rounded-full text-lg  ">
+            Work with me
+          </button>
+        </div>
+      </div>
+      <div className="mt-12">
+        <h2 className="text-3xl text-semibold">
+          My goal as a Frontend web developer is to create memorable,
+          user-centered experiences.
+        </h2>
+        <p className="mt-4 leading-7">
+          Hello, I'm victor olabisi, a dedicated React.js Frontend Developer
+          passionate about crafting seamless user experiences. With a strong
+          foundation in JavaScript and expertise in React.js, I specialize in
+          building responsive and intuitive web applications. I bring a
+          commitment to clean, maintainable code and thrive in collaborative
+          environments. From e-commerce platforms to interactive dashboards, my
+          diverse project experience reflects my ability to find elegant
+          solutions to unique challenges. Committed to continuous learning, I
+          actively contribute to open-source projects and stay on top of the
+          latest industry trends. Let's collaborate and bring innovative ideas
+          to life!{" "}
         </p>
       </div>
-      <div>
-        <h1 className="text-4xl md:text-5xl tracking-wide font-semibold">
-          I craft beautiful and unique products.
-        </h1>
-        <p className="mt-4 leading-8">
-          in my code every details serves a purpose- <br /> orchestrating a
-          digital masterpiece where user experience flow seamlessly{" "}
-        </p>
-        <button className="btn mt-6 btn-primary  rounded-full text-lg  ">Work with me</button>
-      </div>
-    </div>
+    </>
   );
 };
 export default Hero;
