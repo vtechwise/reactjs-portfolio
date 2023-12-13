8c2069f49205125261d181f0906604f37b64b2b4
victor <oguntayovictor65@gmail.com>
2023-12-13 04:00:33 -0800

    fixed img

--- src/component/FeaturedProject.jsx
+++ src/component/FeaturedProject.jsx
@@ -1,16 +1,18 @@
-import SectionTitle from './SectionTitle'
-import thumbnail from '../../public/project-one.jpg'
-
+import SectionTitle from "./SectionTitle";
+// import thumbnail from '../../public/project-one.jpg'
+import comfyImg from "../assets/comfyStore.png";
 const FeaturedProject = () => {
   return (
-      <section className='section-margin'>
-          <SectionTitle text='my creations' />
-          <div>
-              <article>
-                  <img src={thumbnail} alt="" />
-              </article>
+    <section className="section-margin">
+      <SectionTitle text="my creations" />
+      <div>
+        <article>
+          <div className="bg-base-300 rounded-xl">
+            <img src={comfyImg} alt="" />
           </div>
+        </article>
+      </div>
     </section>
-  )
-}
-export default FeaturedProject
+  );
+};
+export default FeaturedProject;
