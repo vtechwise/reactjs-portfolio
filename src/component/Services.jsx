86be6b0851008ab265496fe6747f04b3f328e01b
victor <oguntayovictor65@gmail.com>
2023-12-13 17:15:04 -0800

    started  working on services section

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -1,10 +1,27 @@
-import SectionTitle from "./SectionTitle"
+import { services } from "../utils/data";
+import SectionTitle from "./SectionTitle";
 
 const Services = () => {
   return (
-      <section className="section-margin">
-          <SectionTitle text='services'/>
+    <section className="section-margin">
+      <SectionTitle text="services" />
+      <div className="grid gap-y-10">
+        {services.map((service) => {
+          const { id, title, description } = service;
+          return (
+            <article
+              key={id}
+              className="bg-base-300 border-b-4 border-primary p-10"
+            >
+              <h2 className="text-2xl pb-4 capitalize font-semibold">
+                {title}
+              </h2>
+              <p>{description}</p>
+            </article>
+          );
+        })}
+      </div>
     </section>
-  )
-}
-export default Services
+  );
+};
+export default Services;
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,25 +1,21 @@
-
 export const menus = [
-      {
-        id:1,
-        text:'home',
-
-    }, {
-        id:2,
-        text:'about',
-
-    }, 
-    {
-        id:13,
-        text:'projects',
-
-    },
-     {
-        id:4,
-        text:'services',
-
-    }
-]
+  {
+    id: 1,
+    text: "home",
+  },
+  {
+    id: 2,
+    text: "about",
+  },
+  {
+    id: 13,
+    text: "projects",
+  },
+  {
+    id: 4,
+    text: "services",
+  },
+];
 
 export const featuredProject = [
   {
@@ -36,5 +32,25 @@ export const featuredProject = [
     id: 3,
     title: "mix master",
     image: "/mixMaster (2).png",
+  },
+];
+export const services = [
+  {
+    id: "1",
+    title: "frontend development",
+    description:
+      "Crafting visually appealing and interactive user experiences using HTML, CSS, and JavaScript to enhance the frontend of websites and applications.",
+  },
+  {
+    id: "2",
+    title: "webflow development",
+    description:
+      "Leveraging Webflow's intuitive platform to design and develop responsive websites seamlessly. Transforming ideas into visually stunning and user-friendly web experiences without extensive coding.",
+  },
+  {
+    id: "1",
+    title: "wordpress development",
+    description:
+      "Harnessing the power of WordPress as a content management system to create diverse websites. From blogs to e-commerce platforms, utilizing themes and plugins to customize designs and functionalities for optimal user engagement.",
   },
 ];
