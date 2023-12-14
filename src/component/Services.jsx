1e1d8b06ce56d06ab984d4d19ce4f9c037ff487c
victor <oguntayovictor65@gmail.com>
2023-12-13 17:17:35 -0800

    fix

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -5,13 +5,13 @@ const Services = () => {
   return (
     <section className="section-margin">
       <SectionTitle text="services" />
-      <div className="grid gap-y-10">
+      <div className="grid gap-y-10 sm:grid-cols-2 md:grid-cols-3 gap-x-10">
         {services.map((service) => {
           const { id, title, description } = service;
           return (
             <article
               key={id}
-              className="bg-base-300 border-b-4 border-primary p-10"
+              className="bg-base-300 border-b-4 border-primary p-10 rounded-xl "
             >
               <h2 className="text-2xl pb-4 capitalize font-semibold">
                 {title}
