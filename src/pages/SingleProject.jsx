c486553a4cb1b29d5269265c1a7a71bb61b63b6c
victor <oguntayovictor65@gmail.com>
2023-12-18 16:44:20 -0800

    started working on single project page

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -1,24 +1,56 @@
 import { SectionTitle } from "../component";
-import { singleProjects } from "../utils/data"
+import { singleProjects } from "../utils/data";
 import { useLoaderData } from "react-router-dom";
 
-
-
 export const loader = ({ params }) => {
-    const title = params.id
-    const singleProject = singleProjects.find((project) => {
-       return project.title === title 
-    })
-    return {singleProject}
-}
-
+  const title = params.id;
+  const singleProject = singleProjects.find((project) => {
+    return project.title === title;
+  });
+  return { singleProject };
+};
 
 const SingleProject = () => {
-    const {singleProject} = useLoaderData()
+  const { singleProject } = useLoaderData();
+  const { title, description, url, skills, service } = singleProject;
+  // console.log(skills);
   return (
-      <section className="section-margin">
-          <SectionTitle text={singleProject.title}/>
+    <section className="section-margin">
+      <SectionTitle text={title} />
+      <div className="grid gap-y-16 md:grid-cols-2 gap-x-10">
+        <div className="grid gap-y-2 md:gap-y-0 items-start">
+          <div>
+            <span className="text-xl capitalize tracking-wider font-bold">
+              skills :
+            </span>
+            {skills.map((skill) => {
+              return (
+                <span className="text-lg">{`${
+                  skill.length > 1 ? `${skill} ,` : " "
+                }`}</span>
+              );
+            })}
+          </div>
+          <div>
+            <span className="text-xl capitalize tracking-wider font-bold">
+              service : 
+            </span>
+            <span className="text-lg">{service}</span>
+          </div>
+          <a href={url}>
+            <button className="btn btn-primary rounded-full mt-4">
+              visit website
+            </button>
+          </a>
+        </div>
+        <div className="grid gap-y-6">
+          <h2 className="text-3xl tracking-wider font-semibold">
+            project overview
+          </h2>
+          <p className="leading-8">{description}</p>
+        </div>
+      </div>
     </section>
-  )
-}
-export default SingleProject
+  );
+};
+export default SingleProject;
