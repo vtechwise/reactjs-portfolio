841a971a3da4b7ca41de0f31ee68e7e95f5efaa7
victor <oguntayovictor65@gmail.com>
2023-12-18 18:33:08 -0800

    contact

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,23 @@
-import { Hero, FeaturedProject, Services } from "../component";
-const Landing = () => { 
-    return (
-      <>
-        <Hero />
-        <FeaturedProject />
-        <Services/>
-      </>
-    );
-}
-export default Landing
+import { Hero, FeaturedProject, Services, SectionTitle } from "../component";
+const Landing = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedProject />
+      <Services />
+      <section className="section-margin">
+        <SectionTitle text="want to work" />
+        <div className="bg-base-300 p-8">
+          <p>
+            if you need a modern and powerful website for your business,startup
+            or yourself, i am available for work. you can email me directly at{" "}
+            <span className="link text-primary font-bold text-lg ">
+              victoroguntayo65@gmail.com
+            </span>{" "}
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+export default Landing;
