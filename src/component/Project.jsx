4dc4d995157187fd1297922ed5aa1268e48ae5f4
victor <oguntayovictor65@gmail.com>
2023-12-24 08:24:58 -0800

    sm

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -7,15 +7,14 @@ const Project = ({ image, title, category }) => {
   return (
     <>
       <div>
-        <Link
-          to={`/project/${title}`}
-          className="bg-base-300 rounded-xl group overflow-hidden"
-        >
-          <img
-            src={image}
-            alt=""
-            className="group-hover:scale-105 duration-300 transition"
-          />
+        <Link to={`/project/${title}`}>
+          <div className="bg-base-300 rounded-xl group overflow-hidden">
+            <img
+              src={image}
+              alt=""
+              className="group-hover:scale-105 duration-300 transition"
+            />
+          </div>
         </Link>
         <h3 className="text-xl mt-4 capitalize font-semibold t">{title}</h3>
         {/* <h4>{category}</h4> */}
