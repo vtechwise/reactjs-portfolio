eb9f4a828d057494a7ad078880c4c44232ee7346
victor <oguntayovictor65@gmail.com>
2023-12-28 16:40:02 -0800

    desktop adjustment

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -1,7 +1,7 @@
 import { services } from "../utils/data";
 import SectionTitle from "./SectionTitle";
 import { FaReact } from "react-icons/fa";
-
+import servicesImg from "../assets/services.jpg";
 const Services = () => {
   return (
     <section className="section-margin">
@@ -23,24 +23,53 @@ const Services = () => {
           );
         })}
       </div>
-      <div className="mt-8">
-        <h4 className="capitalize text-lg font-medium mb-4">my daily tools</h4>
-        <div className="grid  justify-start gap-y-2 grid-cols-2">
-          <div className="text-lg capitalize border inline-block py-2 px-4 rounded-full w-36 border-base-300 ">
-            <img src="./figma.png" alt="" className="w-8 inline-block pr-2" />
-            figma
-          </div>
-          <div className="text-lg capitalize border-2 inline-block py-2 px-4 rounded-full border-base-300  w-36">
-            <img src="./vscode.png" alt="" className="w-8 inline-block pr-2" />
-            vs code
-          </div>
-          <div className="text-lg capitalize border-2 inline-block py-2 px-4 rounded-full border-base-300  w-36">
-            <img src="./github.png" alt="" className="w-8 inline-block pr-2" />
-            github
-          </div>{" "}
-          <div className="text-lg capitalize border-2 inline-block py-2 px-4 rounded-full border-base-300  w-36">
-            <img src="./netlify.png" alt="" className="w-8 inline-block pr-2" />
-            netlify
+      <div className="mt-8 grid md:grid-cols-2 mt-24 gap-x-20">
+        <img src={servicesImg} alt="" className="rounded-lg mb-4" />
+        <div className="flex flex-col justify-between gap-y-20">
+          <p className="text-lg">
+            empowered by a versatile tools box and honed skills,i utilized
+            cutting-edge technology to bring digital visions into life. Each
+            toolis carefully selected to elevate projects, ensuring they stand
+            out in the ever-evolving landscape of technology and design.{" "}
+          </p>
+          <div>
+            <h4 className="capitalize text-lg font-medium mb-4">
+              my daily tools
+            </h4>
+            <div className="grid  justify-start gap-y-2 grid-cols-2">
+              <div className="text-lg capitalize border inline-block py-2 px-4 rounded-full w-36 border-base-300 ">
+                <img
+                  src="./figma.png"
+                  alt=""
+                  className="w-8 inline-block pr-2"
+                />
+                figma
+              </div>
+              <div className="text-lg capitalize border-2 inline-block py-2 px-4 rounded-full border-base-300  w-36">
+                <img
+                  src="./vscode.png"
+                  alt=""
+                  className="w-8 inline-block pr-2"
+                />
+                vs code
+              </div>
+              <div className="text-lg capitalize border-2 inline-block py-2 px-4 rounded-full border-base-300  w-36">
+                <img
+                  src="./github.png"
+                  alt=""
+                  className="w-8 inline-block pr-2"
+                />
+                github
+              </div>{" "}
+              <div className="text-lg capitalize border-2 inline-block py-2 px-4 rounded-full border-base-300  w-36">
+                <img
+                  src="./netlify.png"
+                  alt=""
+                  className="w-8 inline-block pr-2"
+                />
+                netlify
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -10,18 +10,91 @@ const Landing = () => {
       <Services />
       <section className="section-margin">
         <SectionTitle text="want to work" />
-        <div className="bg-base-300 p-8 max-w-[30rem]">
-          <p>
-            if you need a modern and powerful website for your business,startup
-            or yourself, i am available for work. you can email me directly at{" "}
-            <span className="link text-primary font-bold text-lg ">
-              victoroguntayo65@gmail.com
-            </span>{" "}
-          </p>
+        <div className="grid md:grid-cols-2 gap-x-20">
+          <div className="bg-base-300 p-8 max-w-[30rem] self-start md:mt-16">
+            <p>
+              if you need a modern and powerful website for your
+              business,startup or yourself, i am available for work. you can
+              email me directly at{" "}
+              <span className="link text-primary font-bold text-lg ">
+                victoroguntayo65@gmail.com
+              </span>{" "}
+            </p>
+          </div>
+          <div className="faq mt-6 hidden md:block">
+            <SectionTitle text="frequently asked question" />
+            <div className="collapse collapse-arrow bg-base-200 ">
+              <input type="radio" name="my-accordion-2" checked="checked" />
+              <div className="collapse-title text-lg font-medium">
+                What technologies are you proficient in for frontend
+                development?{" "}
+              </div>
+              <div className="collapse-content">
+                <p>
+                  I am proficient in HTML, CSS, and JavaScript, which form the
+                  foundation of my frontend development skills. Additionally, I
+                  have hands-on experience with popular frontend libraries and
+                  frameworks such as React.js, enabling me to build interactive
+                  and dynamic user interfaces efficiently.
+                </p>
+              </div>
+            </div>
+            <div className="collapse collapse-arrow bg-base-200 ">
+              <input type="radio" name="my-accordion-2" checked="checked" />
+              <div className="collapse-title text-lg font-medium">
+                Can you describe your approach to responsive web design?
+              </div>
+              <div className="collapse-content">
+                <p>
+                  My approach to responsive web design involves using a
+                  mobile-first strategy. I prioritize the mobile user experience
+                  and then progressively enhance for larger screens. I leverage
+                  flexible grids and media queries to ensure that websites adapt
+                  seamlessly to various devices, providing an optimal viewing
+                  and interaction experience.
+                </p>
+              </div>
+            </div>
+            <div className="collapse collapse-arrow bg-base-200 ">
+              <input type="radio" name="my-accordion-2" checked="checked" />
+              <div className="collapse-title text-lg font-medium">
+                How do you optimize web performance for faster loading times?{" "}
+              </div>
+              <div className="collapse-content">
+                <p>
+                  I optimize web performance by employing several techniques.
+                  This includes code splitting to reduce initial load times,
+                  lazy loading images and components to defer non-essential
+                  content, and minimizing HTTP requests through bundling. I also
+                  prioritize efficient caching strategies and utilize tools like
+                  Lighthouse to identify and address performance bottlenecks.
+                </p>
+              </div>
+            </div>
+            <div className="collapse collapse-arrow bg-base-200 ">
+              <input type="radio" name="my-accordion-2" checked="checked" />
+              <div className="collapse-title text-lg font-medium">
+                What is your experience with version control systems, and which
+                one do you prefer?
+              </div>
+              <div className="collapse-content">
+                <p>
+                  I am well-versed in using Git as my primary version control
+                  system. I have extensive experience with platforms like
+                  GitHub, which facilitates collaborative development and code
+                  review processes. Git enables me to manage code changes
+                  effectively, track project history, and collaborate seamlessly
+                  with team members.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
-      <section className="grid place-items-center pt-6" >
-        <div><p className="capitalize font-semibold">crafted by victor olabisi</p></div>
+      <section className="grid place-items-center pt-6">
+        <div>
+          <p className="capitalize font-semibold">crafted by victor olabisi</p>
+        </div>
       </section>
     </>
   );
