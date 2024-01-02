675cdcf101683f6bf63d38a56e3ca9328f17fce3
victor <oguntayovictor65@gmail.com>
2024-01-02 06:41:07 -0800

    edited resume

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -4,7 +4,7 @@ import { CgMail } from "react-icons/cg";
 import aboutMe from '../assets/aboute me.svg'
 import { Link } from "react-router-dom";
 const Hero = () => {
-    const resumeUrl= 'VICTOR OGUNTAYO CV.pdf'
+    const resumeUrl= 'victor-olabisi cv.pdf'
   return (
     <>
       <div className="flex flex-col gap-y-10 sm:flex-row gap-x-12 items-center pt-14 md:mb-24" id="home">
