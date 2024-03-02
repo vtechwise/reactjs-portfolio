133d7423595a2f77b7a7b31f6d341aeb65647e1d
victor <oguntayovictor65@gmail.com>
2024-03-02 00:40:44 -0800

    error page adjustment

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,10 +4,10 @@ const Error = () => {
   return (
     <div className="grid h-screen place-items-center">
       <div className="text-center -translate-y-10">
-        <p className="text-9xl mb-4 capitalize font-bold">404</p>
+        <p className="text-6xl md:text-9xl mb-4 capitalize font-bold">404</p>
 
-              <h2 className="text-6xl font-bold capitalize">page not found</h2>
-              <p className="text-lg tracking-wider mt-2">sorry we couldn't find te page you are looking</p>
+              <h2 className=" text-xl md:text-5xl font-bold capitalize">page not found</h2>
+              <p className="text-lg  mt-2 capitalize">sorry we could not find the page you are looking for</p>
              <Link to={'/'}> <button className="btn btn-primary text-lg capitalize mt-4">go back home</button> </Link> 
       </div>
     </div>
