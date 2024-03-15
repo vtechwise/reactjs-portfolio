3974081a5af759129cb1985af416178e32eed4f2
victor <oguntayovictor65@gmail.com>
2024-03-15 03:53:10 -0700

    testing

--- src/component/SectionTitle.jsx
+++ src/component/SectionTitle.jsx
@@ -1,6 +1,6 @@
 const SectionTitle = ({text}) => {
   return (
-      <div className="my-8 border-b-4 pb-5 border-base-300 ">
+      <div className="my-6 border-b-4 pb-4 border-base-300 ">
           <h2 className="text-3xl font-bold tracking-wide capitalize">{text}</h2>
     </div>
   )
