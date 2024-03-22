1357f687741ae7f8f3c72eb7c00fa225868a2722
victor <oguntayovictor65@gmail.com>
2024-03-22 15:45:06 -0700

    working on skills

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -2,22 +2,24 @@ import SectionTitle from "./SectionTitle";
 import { BiCodeCurly } from "react-icons/bi";
 import { MdOutlineKeyboardArrowUp } from "react-icons/md";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { useState } from "react";
 
 const Skills = () => {
+  const [showContent, setShowContent] = useState(false)
   return (
     <section>
       <SectionTitle text="skills" />
-      <article className="skill grid grid-cols-[auto,1fr,auto] items-center gap-x-4">
+      <article className="skill grid grid-cols-[auto,1fr,auto] items-center align-start gap-x-4">
         <span>
           <BiCodeCurly className="text-[3rem] fill-primary " />
         </span>
         <h4 className="text-2xl capitalize font-bold">frontend engineeering</h4>
-        <div className="relative mr-6">
+        <div className="relative mr-6 ">
           <span>
-            <MdOutlineKeyboardArrowUp className="text-[3rem] fill-primary absolute" />
+            <MdOutlineKeyboardArrowUp className={`${showContent?'opacity-100':'opacity-0'} text-[3rem] fill-primary absolute translate-y-[-1.2rem]  top-`} />
           </span>
           <span>
-            <MdOutlineKeyboardArrowDown className="text-[3rem] fill-primary absolute" />
+            <MdOutlineKeyboardArrowDown className="text-[3rem] fill-primary absolute top-0 translate-y-[-1.2rem]" />
           </span>
         </div>
       </article>
